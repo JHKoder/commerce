fac753542bbdfe4cebf0306d8bebd3fcbe411c47
JHKoder <jeonghun.kang.dev@gmail.com>
2024-03-28 20:15:29 +0900

    :sparkles: add ErrorCode & Exception

---
+++ src/main/java/github/jhkoder/commerce/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package github.jhkoder.commerce.config.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ErrorResponse> handlerServerException(Exception e) {
+        log.error(e.getMessage(), e);
+        ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(value = ApiException.class)
+    public ResponseEntity<ErrorResponse> handlerApiException(ApiException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode());
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handlerIllegalArgumentExceptionException(IllegalArgumentException e) {
+        log.error(e.getMessage(), e);
+        ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_VALUE);
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    }
+}
