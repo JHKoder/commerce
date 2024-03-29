c47ca5b93ced1ff3bf179d04c01afa91fe3caaef
JHKoder <jeonghun.kang.dev@gmail.com>
2024-03-29 19:05:17 +0900

    :sparkles: apply User security

--- src/main/java/github/jhkoder/commerce/user/domain/User.java
+++ src/main/java/github/jhkoder/commerce/user/domain/User.java
@@ -2,6 +2,8 @@ package github.jhkoder.commerce.user.domain;
 
 import github.jhkoder.commerce.common.entity.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,14 +19,42 @@ public class User extends BaseEntity {
     @Column(name = "id")
     private Long id;
 
-    private String userid;
+    @Size(min=4,max=35)
+    @Column(name = "user_id")
+    private String userId;
+    @Size(min=2,max=28)
+    @Column(name = "user_name")
     private String userName;
     private String password;
+    @Size(min=5,max=255)
     private String email;
-    private String sex;
 
-    /*
-    이메일 인증
-    성명
-     */
+    private String phone;
+
+
+    @Enumerated(value = EnumType.STRING)
+    private Gender gender;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(String userId, String userName, String password, String email, String phone, Gender gender, Role role) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+        this.role = role;
+    }
+
+    @Getter
+    public enum Gender {
+        MAN("남성"),
+        WOMAN("여성");
+        private final String gender;
+
+        Gender(String gender) {
+            this.gender=gender;
+        }
+    }
 }
