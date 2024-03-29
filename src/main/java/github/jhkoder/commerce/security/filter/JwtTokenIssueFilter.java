d4175e8489e3f09ba81ab235e08e2b5c123f775e
JHKoder <jeonghun.kang.dev@gmail.com>
2024-03-29 19:08:12 +0900

    :sparkles: add jwt issue filter

---
+++ src/main/java/github/jhkoder/commerce/security/filter/JwtTokenIssueFilter.java
@@ -0,0 +1,48 @@
+package github.jhkoder.commerce.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import github.jhkoder.commerce.config.exception.AuthenticationCustomException;
+import github.jhkoder.commerce.security.filter.request.LoginRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+import static github.jhkoder.commerce.config.exception.ErrorCode.SECURITY_AUTHENTICATION_METHOD_NOT_SUPPORTED;
+
+public class JwtTokenIssueFilter extends AbstractAuthenticationProcessingFilter {
+    private final ObjectMapper objectMapper;
+
+    public JwtTokenIssueFilter(String defaultFilterProcessesUrl, ObjectMapper objectMapper,
+                               AuthenticationSuccessHandler authenticationSuccessHandler,
+                               AuthenticationFailureHandler authenticationFailureHandler) {
+        super(defaultFilterProcessesUrl);
+        this.objectMapper = objectMapper;
+        this.setAuthenticationSuccessHandler(authenticationSuccessHandler);
+        this.setAuthenticationFailureHandler(authenticationFailureHandler);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException {
+        if (!isPostMethod(request)) {
+            throw new AuthenticationCustomException(SECURITY_AUTHENTICATION_METHOD_NOT_SUPPORTED);
+        }
+
+        var loginRequest = objectMapper.readValue(request.getReader(), LoginRequest.class);
+        var token = UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(), loginRequest.password());
+
+        return this.getAuthenticationManager().authenticate(token);
+    }
+
+    private boolean isPostMethod(HttpServletRequest request) {
+        return HttpMethod.POST.name().equals(request.getMethod());
+    }
+}
