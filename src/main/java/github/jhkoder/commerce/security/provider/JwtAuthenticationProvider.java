0b7f17dca6797e9bd2cff54c5f4528e774795e88
JHKoder <jeonghun.kang.dev@gmail.com>
2024-03-29 19:10:18 +0900

    :sparkles: add security provider

---
+++ src/main/java/github/jhkoder/commerce/security/provider/JwtAuthenticationProvider.java
@@ -0,0 +1,46 @@
+package github.jhkoder.commerce.security.provider;
+
+
+import github.jhkoder.commerce.security.JwtAuthenticationToken;
+import github.jhkoder.commerce.security.service.TokenService;
+import github.jhkoder.commerce.security.service.response.TokenParserResponse;
+import github.jhkoder.commerce.user.domain.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+
+public class JwtAuthenticationProvider implements AuthenticationProvider {
+    private final TokenService tokenService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return authenticate((JwtAuthenticationToken) authentication);
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (JwtAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+
+    private Authentication authenticate(JwtAuthenticationToken authentication) throws AuthenticationException {
+        String jwtToken = authentication.getCredentials();
+        TokenParserResponse response = tokenService.parserToken(jwtToken);
+
+        return new JwtAuthenticationToken(response.username(), authorities(response));
+    }
+
+    private List<SimpleGrantedAuthority> authorities(TokenParserResponse response) {
+        return response.roles().stream()
+                .map(Role::value)
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+}
