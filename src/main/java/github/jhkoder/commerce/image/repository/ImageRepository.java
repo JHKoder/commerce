30e6dbd3c8c61007df932d3ce83bd6b8ce55f149
JHKoder <jeonghun.kang.dev@gmail.com>
2024-04-02 19:59:39 +0900

    :sparkles: add Image repository

---
+++ src/main/java/github/jhkoder/commerce/image/repository/ImageRepository.java
@@ -0,0 +1,11 @@
+package github.jhkoder.commerce.image.repository;
+
+import github.jhkoder.commerce.image.repository.request.ImagePathRequest;
+import github.jhkoder.commerce.image.repository.request.ImageRequest;
+
+import java.io.IOException;
+
+public interface ImageRepository {
+
+    boolean upload(ImageRequest imageRequest, ImagePathRequest pathRequest) throws IOException;
+}
