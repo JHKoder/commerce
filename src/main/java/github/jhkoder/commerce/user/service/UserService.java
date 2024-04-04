f71c956e7da7e89349f69f65b8d3533b00b5960b
JHKoder <jeonghun.kang.dev@gmail.com>
2024-04-05 05:21:45 +0900

    :sparkles: 유저 회원가입 서비스 추가
    1. 회원가입
    2. id 중복조회
    3. 등록전 회원 유무 조회
    4. 고유한 phone/email 중복 조회

---
+++ src/main/java/github/jhkoder/commerce/user/service/UserService.java
@@ -0,0 +1,66 @@
+package github.jhkoder.commerce.user.service;
+
+import github.jhkoder.commerce.exception.ApiException;
+import github.jhkoder.commerce.exception.ErrorCode;
+import github.jhkoder.commerce.signcert.domain.SignCertAuthentication;
+import github.jhkoder.commerce.user.domain.Role;
+import github.jhkoder.commerce.user.domain.User;
+import github.jhkoder.commerce.user.repository.UserRepository;
+import github.jhkoder.commerce.user.service.request.SignUpRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Transactional
+    public void signup(SignUpRequest request) {
+        String encodePassword = passwordEncoder.encode(request.password());
+
+        User user = new User(request.userId(), request.userName(), encodePassword, request.email(), request.phone(), request.gender(), Role.USER);
+
+        userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean isIdCheck(String id) {
+        if (userRepository.findByUserId(id).isEmpty()) {
+            throw new ApiException(ErrorCode.USER_ID_DUPLICATE);
+        }
+        return true;
+    }
+
+    @Transactional(readOnly = true)
+    public void validateMemberRegistration(SignUpRequest request) {
+        if (userRepository.findByUserId(request.userId()).isPresent()) {
+            throw new ApiException(ErrorCode.USER_ID_DUPLICATE);
+        }
+        if (request.authenticationType().equals(SignCertAuthentication.EMAIL) &&
+                userRepository.findByEmail(request.email()).isPresent()) {
+            throw new ApiException(ErrorCode.SIGNUP_EMAIL_DUPLICATE);
+        } else if (userRepository.findByPhone(request.phone()).isPresent()) {
+            throw new ApiException(ErrorCode.SIGNUP_SMS_DUPLICATE);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public void checkSmsValidAndUnique(String phone) {
+        if (!userRepository.existsByPhone(phone)) {
+            throw new ApiException(ErrorCode.USER_PHONE_UNIQUE);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public void checkEmailValidAndUnique(String email) {
+        if (!userRepository.existsByEmail(email)) {
+            throw new ApiException(ErrorCode.USER_EMAIL_UNIQUE);
+        }
+    }
+
+}
