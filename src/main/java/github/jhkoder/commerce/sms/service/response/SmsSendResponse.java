73055592e179b842e0252dc189ec93b4ecc10b19
JHKoder <jeonghun.kang.dev@gmail.com>
2024-04-05 05:40:28 +0900

    :recycle: SMS response record 로 변환 및 수정

--- src/main/java/github/jhkoder/commerce/sms/service/response/SmsSendResponse.java
+++ src/main/java/github/jhkoder/commerce/sms/service/response/SmsSendResponse.java
@@ -5,12 +5,8 @@ import lombok.Data;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 
-@Data
 @JsonAutoDetect(getterVisibility = ANY)
-public class SmsSendResponse {
-    private SmsSendData data;
-    private SmsSendApi api;
+public record SmsSendResponse(SmsSendData data ,SmsSendApi api ) {
     public record SmsSendData(String error,String result,String from,String to,String origin_text,int success){}
-
     public record SmsSendApi(boolean success,int cost,int ms,int pl_id){}
 }
