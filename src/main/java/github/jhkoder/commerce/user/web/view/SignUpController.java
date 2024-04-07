ecc4ec5b8eb8c904ed1ae34bb4bcd0ee0b6a5132
JHKoder <jeonghun.kang.dev@gmail.com>
2024-04-08 01:30:01 +0900

    :recycle: security 수정으로 변경

--- src/main/java/github/jhkoder/commerce/user/web/view/SignUpController.java
+++ src/main/java/github/jhkoder/commerce/user/web/view/SignUpController.java
@@ -1,13 +1,22 @@
 package github.jhkoder.commerce.user.web.view;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Slf4j
 
 @Controller
 public class SignUpController {
 
     @GetMapping("/signup")
     public String signup(){
+
         return "signup";
     }
 }
---
+++ src/main/resources/static/js/cookie.js
@@ -0,0 +1,23 @@
+function setCookie(cookieName, cookieValue, expirationDays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+// 특정 이름의 쿠키 값을 가져오는 함수
+function getCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+    for(var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) == 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
