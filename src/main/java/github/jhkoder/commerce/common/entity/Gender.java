08cb49a350c0aa3477536ddc7c11be8f87d3bb50
JHKoder <jeonghun.kang.dev@gmail.com>
2024-04-09 23:24:16 +0900

    :sparkles: add 성별

---
+++ src/main/java/github/jhkoder/commerce/common/entity/Gender.java
@@ -0,0 +1,14 @@
+package github.jhkoder.commerce.common.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum Gender {
+    MAN("남성"),
+    WOMAN("여성");
+    private final String gender;
+
+    Gender(String gender) {
+        this.gender = gender;
+    }
+}
