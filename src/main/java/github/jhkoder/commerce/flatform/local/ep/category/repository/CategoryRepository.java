34b9c562adf85ff00fd4d4c153c0fbe80903cec3
JHKoder <jeonghun.kang.dev@gmail.com>
2024-04-18 10:58:20 +0900

    :sparkles: 카테고리 리포지토리 추가

--- src/main/java/github/jhkoder/commerce/flatform/local/ep/category/repository/CategoryRepository.java
+++ src/main/java/github/jhkoder/commerce/flatform/local/ep/category/repository/CategoryRepository.java
@@ -1,7 +1,13 @@
 package github.jhkoder.commerce.flatform.local.ep.category.repository;
 
 import github.jhkoder.commerce.flatform.local.ep.category.domain.Category;
+import github.jhkoder.commerce.flatform.local.ep.category.domain.CategoryLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<List<Category>> findByLevel(CategoryLevel level);
 }
