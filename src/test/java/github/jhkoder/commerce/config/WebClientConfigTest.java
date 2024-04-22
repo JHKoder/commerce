7f74e180d24fc410db5fcc98e3f17a64ff2fd404
JHKoder <jeonghun.kang.dev@gmail.com>
2024-04-23 08:49:37 +0900

    :sparkles: api 호출용 webclient 추가

---
+++ src/test/java/github/jhkoder/commerce/config/WebClientConfigTest.java
@@ -0,0 +1,37 @@
+package github.jhkoder.commerce.config;
+
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
+
+public class WebClientConfigTest {
+
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 100_000)
+                .responseTimeout(Duration.ofMillis(100_000))
+                .doOnConnected(conn -> {
+                    conn.addHandlerLast(new ReadTimeoutHandler(100_000, TimeUnit.MILLISECONDS))
+                            .addHandlerLast(new WriteTimeoutHandler(100_000, TimeUnit.MILLISECONDS));
+                });
+
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .defaultHeader(CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON.toString())
+                .defaultHeader(ACCEPT, HttpHeaderValues.APPLICATION_JSON.toString())
+                .build();
+    }
+}
