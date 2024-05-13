1a4ef82547c7b555894656742112340546c4809e
JHKoder <jeonghun.kang.dev@gmail.com>
2024-05-13 23:49:56 +0900

    :sparkles: 상품 페이징 api

--- src/main/java/github/jhkoder/commerce/flatform/local/ep/itemproduct/web/rest/ProductApiController.java
+++ src/main/java/github/jhkoder/commerce/flatform/local/ep/itemproduct/web/rest/ProductApiController.java
@@ -1,6 +1,10 @@
 package github.jhkoder.commerce.flatform.local.ep.itemproduct.web.rest;
 
+import github.jhkoder.commerce.flatform.local.ep.category.domain.Category;
+import github.jhkoder.commerce.flatform.local.ep.category.repository.dto.CategoryDto;
+import github.jhkoder.commerce.flatform.local.ep.category.service.CategoryService;
 import github.jhkoder.commerce.flatform.local.ep.itemproduct.service.ProductService;
+import github.jhkoder.commerce.flatform.local.ep.itemproduct.service.dto.CategoryChildDto;
 import github.jhkoder.commerce.flatform.local.ep.itemproduct.service.request.ProductCategoryPageRequest;
 import github.jhkoder.commerce.flatform.local.ep.itemproduct.service.response.ProductResponse;
 import github.jhkoder.commerce.flatform.local.ep.itemproduct.service.response.ProductsCategoryPageResponse;
@@ -9,16 +13,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/all/products")
+@RequestMapping("/all/products")
 @RequiredArgsConstructor
 public class ProductApiController {
     private final ProductService productService;
+    private final CategoryService categoryService;
 
     // 특정 카테고리 페이징
-    @GetMapping("/category")
+    @PostMapping("/category")
     public Page<ProductsCategoryPageResponse> category(@Valid @RequestBody ProductCategoryPageRequest request){
-        return productService.findCategoryPage(request) ;
+        String categoryPath = categoryService.findByIdAndChild(request.categoryId());
+        return productService.findCategoryPage(categoryPath,request) ;
     }
 
     // 상품 상세보기
