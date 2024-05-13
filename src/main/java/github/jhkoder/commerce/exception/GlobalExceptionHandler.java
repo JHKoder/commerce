3fb8607842b8f7d51ee80db49eb56e7e32a4c54a
JHKoder <jeonghun.kang.dev@gmail.com>
2024-05-14 00:13:28 +0900

    :sparkles: Global Exception handler update

--- src/main/java/github/jhkoder/commerce/exception/GlobalExceptionHandler.java
+++ src/main/java/github/jhkoder/commerce/exception/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package github.jhkoder.commerce.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handlerServerException(Exception e) {
